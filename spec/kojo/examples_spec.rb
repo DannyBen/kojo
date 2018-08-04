05f3b6e93ee91463663ddc5b3db7dd832d85a062
Danny Ben Shitrit <db@dannyben.com>
2018-08-04 08:35:09 +0000

    add save to file examples

--- spec/kojo/examples_spec.rb
+++ spec/kojo/examples_spec.rb
@@ -11,6 +11,23 @@ describe 'examples' do
       end
       puts "  --> #{name}"
       expect(result).to match_fixture("examples/#{name}")
+
+      result_file = "#{example}/result.yml"
+      result_dir  = "#{example}/result"
+
+      if File.exist? result_file
+        puts "    > Verifying result file"
+        expect(File.read result_file).to match_fixture("examples/results/#{name}")
+      end
+
+      if Dir.exist? result_dir
+        puts "    > Verifying result dir"
+        Dir["#{result_dir}/*"].each do |file|
+          filename = File.basename file
+          puts "    > Verifying #{filename}"
+          expect(File.read file).to match_fixture("examples/results/#{filename}")
+        end
+      end
     end
   end  
 end
