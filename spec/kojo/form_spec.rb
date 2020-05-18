5b1a1cea84b219efd976b94b9ce087bc9904b5b9
Danny Ben Shitrit <db@dannyben.com>
2020-05-18 17:03:24 +0000

    fix kwargs warnings for ruby 2.7

--- spec/kojo/form_spec.rb
+++ spec/kojo/form_spec.rb
@@ -25,14 +25,38 @@ describe Kojo::Form do
     end
   end
 
+  describe '#warn' do
+    let(:warning) { "Beware the door with too many keys" }
+
+    it "delegates to TTY::Prompt" do
+      expect(subject.prompt).to receive(:warn).with warning
+      subject.warn warning
+    end
+  end
+
   describe '#method_missing' do
     it "forwards calls to a TTY::Prompt instance" do
       expect(subject.prompt).to receive(:hello)
       subject.hello
     end
 
-    it "raises an error when TTY::Prompt does not respond to this method" do
-      expect { subject.hello }.to raise_error(NoMethodError)
+    context "with positional args and keyword args" do
+      it "works" do
+        result = nil
+
+        supress_output do
+          send_input "" do
+            expect { subject.select("Choose your destiny?", %w(Scorpion Kano Jax), cycle: true) }
+              .to output(/Choose your destiny/).to_stdout
+          end
+        end
+      end
+    end
+
+    context "when the method does not exist in TTY::Prompt" do
+      it "raises an error" do
+        expect { subject.hello }.to raise_error(NoMethodError)
+      end
     end
   end
 end
